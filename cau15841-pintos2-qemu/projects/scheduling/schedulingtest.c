6c829be51ae59ea3a6e3880ba8fff5a74dc14639
cauprkprj <comnamu18@gmail.com>
2018-06-12 23:01:32 +0900

    last version / comment added / make clean

--- cau15841-pintos2-qemu/projects/scheduling/schedulingtest.c
+++ cau15841-pintos2-qemu/projects/scheduling/schedulingtest.c
@@ -49,6 +49,8 @@ void run_scheduling_test(char **argv UNUSED)
 	for (i=0; i<MAX_THREAD_CNT; i++) {
 		struct thread_info *ti = &info[i];
 		char name[16];
+		// Thread index : 0 1 2 3 4 5 6 7 8 9
+		// priority     : 4 3 2 1 0 4 3 2 1 0
 		int priority = PRI_MAX - i%(PRI_MAX+1);
 
 		ti->id = i;
@@ -65,6 +67,7 @@ void run_scheduling_test(char **argv UNUSED)
 	for (i=0; i<MAX_THREAD_CNT; i++) {
 		sema_down(&info[i].sema_join);
 	}
+	// Print result
 	for (i=0; i<MAX_THREAD_CNT; i++) {
 		printf("Thread id : %d, initialized priority : %d, received ticks :%d\n", i,PRI_MAX - i%(PRI_MAX+1), info[i].tick_count);
 	}
